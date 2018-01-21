58412e9a4e78e3e3880353558bc6a156366adec9
kev-landry <kev.landry@free.fr>
2018-01-21 13:22:24 +0100

    renaming

--- wp-content/themes/devkick/index.php
+++ wp-content/themes/devkick/index.php
@@ -6,14 +6,14 @@
                         <div class="header-left column is-two-thirds">
                             <h1 class="title is-3">Bienvenu sur KevToDev</h1>
                             <p class="subtitle is-5">27 ans, actuellement en alternance en tant que developpeur web et passionné par les nouvelles
-                                technologies, notamment du web et de l'IA. Ici tu trouvera ce que j'apprend au fil de mon
+                                technologies, notamment du web et de l'IA. Ici tu trouveraa ce que j'apprend au fil de mon
                                 apprentissage et découverte sous forme d'articles/tutos. Je ferai donc en sorte, à mon échelle,
                                 de leur rendre la pareille via ce blog .
                             </p>
                         </div>
                         <div class="header-right column">
                             <figure class="image is-256x256">
-                                <img src="//localhost:3000/Devkick-wordpress-theme/wp-content/uploads/2018/01/cercle_moi.jpg" alt="avatar">
+                                <img src="//kevtodev.loc:3000/wp-content/uploads/2018/01/cercle_moi.jpg" alt="avatar">
                             </figure>
                         </div>
                     </div>
