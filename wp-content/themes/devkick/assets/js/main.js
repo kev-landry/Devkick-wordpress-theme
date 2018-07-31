284973d6c8517be9d635cfad273b15862ee56896
kevin <kevin@vps330068.ovh.net>
2018-07-31 22:57:58 +0200

    fix hello world main.js

--- wp-content/themes/devkick/assets/js/main.js
+++ wp-content/themes/devkick/assets/js/main.js
@@ -1,7 +1,7 @@
 console.log(
     "Hello world !"
-+   "\n\nSi tu t'amuses à inspecter sache que j'ai ce créé"
-+   "\nce theme avec le framework css bulma que j\'ai"
++   "\n\nSi tu t'amuses à inspecter sache que j'ai créé ce"
++   "\nce thème avec le framework css bulma que j\'ai"
 +   "\nensuite custom pour utiliser entre autre grid."
 );
 
